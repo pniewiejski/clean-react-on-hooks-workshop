a9aac03d036727607f682cb2598cbd3f938c5289
Piotr Niewiejski <piotrniewiejski@gmail.com>
2020-02-23 11:18:52 +0100

    Completed 03/01

--- src/03/01.basics.js
+++ src/03/01.basics.js
@@ -2,9 +2,23 @@ import React, {useEffect, useState} from 'react'
 
 export const Counter = function({saveCounterLocally, getInitialCounter, persistCounter}) {
   // your Counter implementation(s) here
+  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    (async () => {
+      const initialCounter = await getInitialCounter();
+      setCount(initialCounter);
+    })()
+  }, [])
+
+  useEffect(() => { saveCounterLocally(count) }, [count])
+
+  useEffect(() => {
+    persistCounter(count)
+  }, [count])
 
   return <div>
-    <p>{/* count here */}</p>
-    <button onClick={() => {/* handler here */}}>count</button>
+    <p>{count}</p>
+    <button onClick={() => { setCount(c => c + 1) } }>count</button>
   </div>
 }
