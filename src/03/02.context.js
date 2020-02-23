b35ef77805ae321ab0da2e1fe07901daa4bbc270
Piotr Niewiejski <piotrniewiejski@gmail.com>
2020-02-23 11:44:09 +0100

    Completed 03/02

--- src/03/02.context.js
+++ src/03/02.context.js
@@ -1,19 +1,21 @@
 import React, {useState, useContext} from 'react'
 
 // initialize react context, with incrementation as default value
-const CounterContext = null
+const CounterContext = React.createContext(state => state + 1)
 
 const ContextAwareCounter = function() {
   // instructions:
   // take the state transition from context (useContext)
   // use context function to change the counter state
+  const stateTransition = useContext(CounterContext)
 
   // you should consider: [someState, setSomeState] = useState() hook
   // hint: there is an other way to call the useState hook...
+  const [count, setCount] = useState(0)
 
   return <div>
-    <p>{/* count here */}</p>
-    <button onClick={() => {/* handler here */}}>count</button>
+    <p>{count}</p>
+    <button onClick={() => { setCount(stateTransition) }}>count</button>
   </div>
 }
 
