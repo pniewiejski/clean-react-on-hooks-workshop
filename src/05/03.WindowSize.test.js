6049692a24f510c420e3780fa7b6fb38d873789c
Piotr Niewiejski <piotrniewiejski@gmail.com>
2020-03-07 21:45:41 +0100

    Completed 05/03 (WindowSize)

--- src/05/03.WindowSize.test.js
+++ src/05/03.WindowSize.test.js
@@ -2,18 +2,19 @@ import React from 'react'
 import {render, fireEvent} from '@testing-library/react'
 import WindowSize from './WindowSize'
 
-describe.skip(`WindowSize`, () => {
+describe(`WindowSize`, () => {
   it(`should render with default resolution`, () => {
     // given
     const DEFAULT_DOM_RESOLUTION = '1024x768'
 
     // when
     // rendering WindowSize
+    const { getByText } = render(<WindowSize/>)
     // you search by default resolution text
-
+    const text = getByText(DEFAULT_DOM_RESOLUTION)
     // then
     // the element is there
-
+    expect(text).not.toEqual(null)
     // and there are diffrent kinds of api you could use (if you didn't know already)
     // https://testing-library.com/docs/react-testing-library/cheatsheet
   })
@@ -24,13 +25,18 @@ describe.skip(`WindowSize`, () => {
     const newHeight = 800
     const resolution = `${newWidth}x${newHeight}`
     // WindowSize is rendered
-
+    const { getByText } = render(<WindowSize/>)
+    
     // when
     // firing window resize event
-    // when you search by new resolution text
-
+    window.innerWidth = newWidth;
+    window.innerHeight = newHeight;
+    fireEvent(window, new Event('resize'))
+    
     // then
+    // when you search by new resolution text
     // the element is there
+    expect(getByText(resolution)).not.toEqual(null)
   })
 })
 
